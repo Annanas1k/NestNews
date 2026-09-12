9d041a8d2f9efe716e1d8330225bbfe62f39ce73
radisimo <radu.pavlovschi@gmail.com>
2026-09-12 09:20:01 +0300

    create module structure (controller, servide, module) for 4 tables: articles, categories, comments, users

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { HealthController } from './health.controller.js';
 import { PrismaService } from './prisma.service.js';
+import { ArticlesModule } from './articles/articles.module';
+import { CategoriesModule } from './categories/categories.module';
+import { CommentsModule } from './comments/comments.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [ArticlesModule, CategoriesModule, CommentsModule, UsersModule],
   controllers: [AppController, HealthController],
   providers: [AppService, PrismaService],
 })
